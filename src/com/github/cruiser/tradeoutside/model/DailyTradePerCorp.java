85b17dfd2f1a27369f63b30e0ffb1a59c058ec94
Qiming Gu <cruiser31@gmail.com>
2013-04-09 23:40:10 +0800

    增加hbm文件

--- src/com/github/cruiser/tradeoutside/model/DailyTradePerCorp.java
+++ src/com/github/cruiser/tradeoutside/model/DailyTradePerCorp.java
@@ -1,9 +1,11 @@
 package com.github.cruiser.tradeoutside.model;
 
 import java.io.Serializable;
+
 import java.math.BigDecimal;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 
 public class DailyTradePerCorp implements Serializable {
 	
@@ -21,24 +23,127 @@ public class DailyTradePerCorp implements Serializable {
 	private Map<String, BigDecimal> termTxnAmts;/* 终端金额汇总 */
 
 
+	public long getId() {
+		return id;
+	}
+
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+
+	public Corp getCorp() {
+		return corp;
+	}
+
+
+	public void setCorp(Corp corp) {
+		this.corp = corp;
+	}
+
+
+	public String getActDat() {
+		return actDat;
+	}
+
+
+	public void setActDat(String actDat) {
+		this.actDat = actDat;
+	}
+
+
+	public BigDecimal getTotalDccRate() {
+		return totalDccRate;
+	}
+
+
+	public void setTotalDccRate(BigDecimal totalDccRate) {
+		this.totalDccRate = totalDccRate;
+	}
+
+
+	public BigDecimal getTotalEdcRate() {
+		return totalEdcRate;
+	}
+
+
+	public void setTotalEdcRate(BigDecimal totalEdcRate) {
+		this.totalEdcRate = totalEdcRate;
+	}
+
+
+	public BigDecimal getTotalTxnAmt() {
+		return totalTxnAmt;
+	}
+
+
+	public void setTotalTxnAmt(BigDecimal totalTxnAmt) {
+		this.totalTxnAmt = totalTxnAmt;
+	}
+
+
+	public Map<String, BigDecimal> getTermDccRates() {
+		return termDccRates;
+	}
+
+
+	public void setTermDccRates(Map<String, BigDecimal> termDccRates) {
+		this.termDccRates = termDccRates;
+	}
+
+
+	public Map<String, BigDecimal> getTermEdcRates() {
+		return termEdcRates;
+	}
+
+
+	public void setTermEdcRates(Map<String, BigDecimal> termEdcRates) {
+		this.termEdcRates = termEdcRates;
+	}
+
+
+	public Map<String, BigDecimal> getTermTxnAmts() {
+		return termTxnAmts;
+	}
+
+
+	public void setTermTxnAmts(Map<String, BigDecimal> termTxnAmts) {
+		this.termTxnAmts = termTxnAmts;
+	}
+
+
 	public String toString(){
 		StringBuffer output = new StringBuffer();
 		output.append("商户每天交易统计：（")
 		.append("\n商户号: ").append(corp.getBusiNo())
-		.append("\n会计日期: ").append(dccRate)
-		.append("\nedc手续费率: ").append(edcRate);
+		.append("\n会计日期: ").append(actDat)
+		.append("\ndcc手续费汇总: ").append(totalDccRate)
+		.append("\nedc手续费汇总: ").append(totalEdcRate)
+		.append("\n交易总金额: ").append(totalTxnAmt);
 
-		Iterator<String> commonIt = dccTerminals.iterator();
+		Set<String> keySet = termDccRates.keySet();
+		Iterator<String> commonIt = keySet.iterator();
 		while(commonIt.hasNext()){
-			output.append("\ndcc终端编号: ").append(commonIt.next());
+			String termNo = commonIt.next();
+			output.append("\ndcc终端").append(termNo).append("手续费汇总: ").append(termDccRates.get(termNo));
 		}
 
-		commonIt = edcTerminals.iterator();
+		keySet = termEdcRates.keySet();
+		commonIt = keySet.iterator();
 		while(commonIt.hasNext()){
-			output.append("\nedc终端编号: ").append(commonIt.next());
+			String termNo = commonIt.next();
+			output.append("\nedc终端").append(termNo).append("手续费汇总: ").append(termEdcRates.get(termNo));
 		}
 
-		output.append("）");
+		keySet = termTxnAmts.keySet();
+		commonIt = keySet.iterator();
+		while(commonIt.hasNext()){
+			String termNo = commonIt.next();
+			output.append("\nedc终端").append(termNo).append("手续费汇总: ").append(termTxnAmts.get(termNo));
+		}
+
+
 		return output.toString();
 
 	}
